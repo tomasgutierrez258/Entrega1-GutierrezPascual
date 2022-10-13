edca06222305618833f7342ce3de90558ed33352
Tomas Gutierrez <tomasgutierrez9912@gmail.com>
2022-10-12 21:43:49 -0300

    Agregando vistas y base del proyecto

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
+from django.template import Context,Template,loader
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, "home/index.html")
+
+def about(request):
+    return render(request, "home/about.html")
